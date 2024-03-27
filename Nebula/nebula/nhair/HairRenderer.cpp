4982dfbed97199e58a6c6780f8beb9889297a1f9
Andromeda08 <vkovacsbalazs26@gmail.com>
2024-03-27 15:26:52 +0100

    New RenderPass

--- Nebula/nebula/nhair/HairRenderer.cpp
+++ Nebula/nebula/nhair/HairRenderer.cpp
@@ -8,10 +8,6 @@ namespace Nebula::nhair
     HairRenderer::HairRenderer(const std::shared_ptr<nvk::Device>& device,
                                const std::shared_ptr<nvk::Swapchain>& swapchain)
     {
-        m_render_res = swapchain->extent();
-        m_clear_values[0].setColor(std::array{ 0.0f, 0.0f, 0.0f, 1.0f });
-        m_clear_values[1].setDepthStencil({ 1.0f, 0 });
-
         auto depth_info = nvk::ImageCreateInfo()
             .set_aspect_flags(vk::ImageAspectFlagBits::eDepth)
             .set_extent(swapchain->extent())
@@ -20,12 +16,12 @@ namespace Nebula::nhair
             .set_name("Hair Depth Buffer");
         m_depth = std::make_shared<nvk::Image>(depth_info, device);
 
-        m_render_pass = nvk::RenderPass::Builder()
+        auto render_pass_create_info = nvk::RenderPassCreateInfo()
             .add_color_attachment(swapchain->format(), vk::ImageLayout::ePresentSrcKHR)
-            .set_depth_attachment(m_depth->properties().format)
-            .make_subpass()
-            .with_name("Hair")
-            .create(device);
+            .set_depth_attachment(m_depth)
+            .set_render_area({{0, 0}, swapchain->extent()})
+            .set_name("Hair");
+        m_render_pass = std::make_shared<nvk::RenderPass>(render_pass_create_info, device);
 
          auto descriptor_create_info = nvk::DescriptorCreateInfo()
             .add(Nebula::nvk::DescriptorType::eUniformBuffer, 0, vk::ShaderStageFlagBits::eMeshEXT | vk::ShaderStageFlagBits::eFragment)
@@ -34,8 +30,7 @@ namespace Nebula::nhair
             .enable_ring_mode();
         m_descriptor = std::make_shared<nvk::Descriptor>(descriptor_create_info, device);
 
-        nvk::PipelineCreateInfo pipeline_create_info;
-        pipeline_create_info
+        auto pipeline_create_info = nvk::PipelineCreateInfo()
             .add_push_constant({ vk::ShaderStageFlagBits::eTaskEXT | vk::ShaderStageFlagBits::eMeshEXT, 0, sizeof(HairConstants) })
             .add_descriptor_set_layout(m_descriptor->layout())
             .set_pipeline_type(nvk::PipelineType::eGraphics)
@@ -44,7 +39,7 @@ namespace Nebula::nhair
             .add_shader("hair.frag.spv", vk::ShaderStageFlagBits::eFragment)
             .add_attachment(true)
             .set_cull_mode(vk::CullModeFlagBits::eNone)
-            .set_render_pass(m_render_pass)
+            .set_render_pass(m_render_pass->render_pass())
             .set_name("Hair");
         m_pipeline = std::make_shared<nvk::Pipeline>(pipeline_create_info, device);
 
@@ -53,7 +48,7 @@ namespace Nebula::nhair
             .add_attachment(swapchain->image_view(0), 0, 0)
             .add_attachment(swapchain->image_view(1), 0, 1)
             .add_attachment(m_depth->image_view(), 1)
-            .set_render_pass(m_render_pass)
+            .set_render_pass(m_render_pass->render_pass())
             .set_extent(swapchain->extent())
             .set_name("Hair Renderer")
             .validate();
@@ -70,6 +65,12 @@ namespace Nebula::nhair
                 .set_size(sizeof(ns::CameraData));
 
             m_uniform_buffers[i] = std::make_shared<nvk::Buffer>(buf_create_info, device);
+
+            vk::DescriptorBufferInfo buffer_info = { m_uniform_buffers[i]->buffer(), 0, sizeof(ns::CameraData)};
+            auto write_info = nvk::DescriptorWriteInfo()
+                .set_set_index(i)
+                .add_uniform_buffer(0, buffer_info);
+            m_descriptor->write(write_info);
         }
     }
 
@@ -84,33 +85,21 @@ namespace Nebula::nhair
         command_buffer.beginDebugUtilsLabelEXT(&marker);
 
         m_uniform_buffers[current_frame]->set_data(&camera_data);
+        m_render_pass->execute(command_buffer, m_framebuffers->get(current_frame), [&](const vk::CommandBuffer& cmd) {
+            m_pipeline->bind(cmd);
+            cmd.bindDescriptorSets(vk::PipelineBindPoint::eGraphics, m_pipeline->layout(), 0, 1, &m_descriptor->set(current_frame), 0, nullptr);
 
-        vk::DescriptorBufferInfo buffer_info = { m_uniform_buffers[current_frame]->buffer(), 0, sizeof(ns::CameraData)};
-        auto write_info = nvk::DescriptorWriteInfo()
-            .set_set_index(current_frame)
-            .add_uniform_buffer(0, buffer_info);
-        m_descriptor->write(write_info);
+            auto buffer_addresses = hair_model.get_hair_buffer_addresses();
+            HairConstants push_constant {
+                .model = glm::rotate(glm::mat4(1.0f), glm::radians(-90.0f), glm::vec3(1, 0, 0)),
+                .buffer_lengths = glm::ivec4(hair_model.vertex_count(), hair_model.strand_count(), 0, 0),
+                .vertex_buffer = buffer_addresses.vertex_buffer,
+                .strand_descriptions_buffers = buffer_addresses.strand_descriptions_buffer,
+            };
 
-        nvk::RenderPass::Execute()
-            .with_clear_values<2>(m_clear_values)
-            .with_framebuffer(m_framebuffers->get(current_frame))
-            .with_render_area({ {0, 0}, m_render_res })
-            .with_render_pass(m_render_pass)
-            .execute(command_buffer, [&](const vk::CommandBuffer& cmd) {
-                m_pipeline->bind(cmd);
-                cmd.bindDescriptorSets(vk::PipelineBindPoint::eGraphics, m_pipeline->layout(), 0, 1, &m_descriptor->set(current_frame), 0, nullptr);
-
-                auto buffer_addresses = hair_model.get_hair_buffer_addresses();
-                const HairConstants push_constant {
-                    .model = glm::rotate(glm::mat4(1.0f), glm::radians(-90.0f), glm::vec3(1, 0, 0)),
-                    .buffer_lengths = glm::ivec4(hair_model.vertex_count(), hair_model.strand_count(), 0, 0),
-                    .vertex_buffer = buffer_addresses.vertex_buffer,
-                    .strand_descriptions_buffers = buffer_addresses.strand_descriptions_buffer,
-                };
-
-                cmd.pushConstants(m_pipeline->layout(), vk::ShaderStageFlagBits::eTaskEXT | vk::ShaderStageFlagBits::eMeshEXT, 0, sizeof(HairConstants), &push_constant);
-                hair_model.draw(cmd);
-            });
+            cmd.pushConstants(m_pipeline->layout(), vk::ShaderStageFlagBits::eTaskEXT | vk::ShaderStageFlagBits::eMeshEXT, 0, sizeof(HairConstants), &push_constant);
+            hair_model.draw(cmd);
+        });
 
         command_buffer.endDebugUtilsLabelEXT();
     }
--- Nebula/nvk/src/render/RenderPass.cpp
+++ Nebula/nvk/src/render/RenderPass.cpp
@@ -148,4 +148,192 @@ namespace Nebula::nvk
         fn(cmd);
         cmd.endRenderPass();
     }
+
+    RenderPassCreateInfo&
+    RenderPassCreateInfo::add_color_attachment(vk::Format format, vk::ImageLayout final_layout, vk::SampleCountFlagBits sample_count,
+                                              vk::ClearColorValue clear_value, vk::AttachmentLoadOp load_op)
+    {
+        auto ad = vk::AttachmentDescription()
+            .setFormat(format)
+            .setSamples(sample_count)
+            .setLoadOp(load_op)
+            .setStoreOp(vk::AttachmentStoreOp::eStore)
+            .setStencilLoadOp(vk::AttachmentLoadOp::eDontCare)
+            .setStencilStoreOp(vk::AttachmentStoreOp::eDontCare)
+            .setInitialLayout(vk::ImageLayout::eUndefined)
+            .setFinalLayout(final_layout);
+        attachments.push_back(ad);
+
+        auto ref = vk::AttachmentReference()
+            .setLayout(vk::ImageLayout::eColorAttachmentOptimal)
+            .setAttachment(attachments.size() - 1);
+        color_refs.push_back(ref);
+
+        auto cv = vk::ClearValue(clear_value);
+        clear_values.push_back(cv);
+
+        return *this;
+    }
+
+    RenderPassCreateInfo&
+    RenderPassCreateInfo::add_attachment(const std::shared_ptr<Image>& image, vk::ImageLayout final_layout,
+                                         vk::ClearColorValue clear_value, vk::AttachmentLoadOp load_op)
+    {
+        auto ad = vk::AttachmentDescription()
+            .setFormat(image->properties().format)
+            .setSamples(image->properties().sample_count)
+            .setLoadOp(load_op)
+            .setStoreOp(vk::AttachmentStoreOp::eStore)
+            .setStencilLoadOp(vk::AttachmentLoadOp::eDontCare)
+            .setStencilStoreOp(vk::AttachmentStoreOp::eDontCare)
+            .setInitialLayout(vk::ImageLayout::eUndefined)
+            .setFinalLayout(final_layout);
+        attachments.push_back(ad);
+
+        auto ref = vk::AttachmentReference()
+            .setLayout(vk::ImageLayout::eColorAttachmentOptimal)
+            .setAttachment(attachments.size() - 1);
+        color_refs.push_back(ref);
+
+        auto cv = vk::ClearValue(clear_value);
+        clear_values.push_back(cv);
+
+        return *this;
+    }
+
+    RenderPassCreateInfo&
+    RenderPassCreateInfo::set_depth_attachment(vk::Format format, vk::SampleCountFlagBits sample_count,
+                                               vk::ClearDepthStencilValue clear_value)
+    {
+        auto ad = vk::AttachmentDescription()
+            .setFormat(format)
+            .setSamples(sample_count)
+            .setLoadOp(vk::AttachmentLoadOp::eClear)
+            .setStoreOp(vk::AttachmentStoreOp::eDontCare)
+            .setStencilLoadOp(vk::AttachmentLoadOp::eDontCare)
+            .setStencilStoreOp(vk::AttachmentStoreOp::eDontCare)
+            .setInitialLayout(vk::ImageLayout::eUndefined)
+            .setFinalLayout(vk::ImageLayout::eDepthStencilAttachmentOptimal);
+        attachments.push_back(ad);
+
+        has_depth_attachment = true;
+        depth_ref.setAttachment(attachments.size() - 1);
+        depth_ref.setLayout(vk::ImageLayout::eDepthStencilAttachmentOptimal);
+
+        auto cv = vk::ClearValue();
+        cv.depthStencil = clear_value;
+        clear_values.push_back(cv);
+
+        return *this;
+    }
+
+    RenderPassCreateInfo&
+    RenderPassCreateInfo::set_depth_attachment(const std::shared_ptr<Image>& depth_image, vk::ClearDepthStencilValue clear_value)
+    {
+        auto ad = vk::AttachmentDescription()
+            .setFormat(depth_image->properties().format)
+            .setSamples(depth_image->properties().sample_count)
+            .setLoadOp(vk::AttachmentLoadOp::eClear)
+            .setStoreOp(vk::AttachmentStoreOp::eDontCare)
+            .setStencilLoadOp(vk::AttachmentLoadOp::eDontCare)
+            .setStencilStoreOp(vk::AttachmentStoreOp::eDontCare)
+            .setInitialLayout(vk::ImageLayout::eUndefined)
+            .setFinalLayout(vk::ImageLayout::eDepthStencilAttachmentOptimal);
+        attachments.push_back(ad);
+
+        has_depth_attachment = true;
+        depth_ref.setAttachment(attachments.size() - 1);
+        depth_ref.setLayout(vk::ImageLayout::eDepthStencilAttachmentOptimal);
+
+        auto cv = vk::ClearValue();
+        cv.depthStencil = clear_value;
+        clear_values.push_back(cv);
+
+        return *this;
+    }
+
+    RenderPassCreateInfo& RenderPassCreateInfo::set_name(const std::string& value)
+    {
+        name = value;
+        return *this;
+    }
+
+    RenderPassCreateInfo& RenderPassCreateInfo::set_render_area(const vk::Rect2D& value)
+    {
+        render_area = value;
+        return *this;
+    }
+
+    RenderPass::RenderPass(const RenderPassCreateInfo& create_info, const std::shared_ptr<Device>& device)
+    : m_device(device), m_render_area(create_info.render_area), m_clear_values(create_info.clear_values)
+    {
+        auto subpass = vk::SubpassDescription()
+            .setColorAttachmentCount(static_cast<uint32_t>(create_info.color_refs.size()))
+            .setInputAttachmentCount(0)
+            .setPInputAttachments(nullptr)
+            .setPResolveAttachments(create_info.has_resolve_attachment ? &create_info.resolve_ref : nullptr)
+            .setPColorAttachments(create_info.color_refs.data())
+            .setPDepthStencilAttachment(create_info.has_depth_attachment ? &create_info.depth_ref : nullptr)
+            .setPipelineBindPoint(vk::PipelineBindPoint::eGraphics);
+
+        auto subpass_dependency = vk::SubpassDependency()
+            .setSrcSubpass(VK_SUBPASS_EXTERNAL)
+            .setDstSubpass(0)
+            .setSrcStageMask(vk::PipelineStageFlagBits::eColorAttachmentOutput | vk::PipelineStageFlagBits::eEarlyFragmentTests)
+            .setSrcAccessMask({})
+            .setDstStageMask(vk::PipelineStageFlagBits::eColorAttachmentOutput | vk::PipelineStageFlagBits::eEarlyFragmentTests)
+            .setDstAccessMask(vk::AccessFlagBits::eColorAttachmentWrite | vk::AccessFlagBits::eDepthStencilAttachmentWrite);
+
+
+        auto rp_create_info = vk::RenderPassCreateInfo()
+            .setAttachmentCount(create_info.attachments.size())
+            .setPAttachments(create_info.attachments.data())
+            .setSubpassCount(1)
+            .setPSubpasses(&subpass)
+            .setDependencyCount(1)
+            .setPDependencies(&subpass_dependency);
+
+        if (const vk::Result result = m_device->handle().createRenderPass(&rp_create_info, nullptr, &m_render_pass);
+            result != vk::Result::eSuccess)
+        {
+            throw nlog::make_exception("Failed to create vk::RenderPass {}: {}", create_info.name, to_string(result));
+        }
+
+        m_device->name_object(std::format("[RP] {}", create_info.name),
+                              (uint64_t) m_render_pass.operator VkRenderPass(),
+                              vk::ObjectType::eRenderPass);
+
+        m_begin_info = vk::RenderPassBeginInfo()
+            .setRenderArea(m_render_area)
+            .setRenderPass(m_render_pass)
+            .setClearValueCount(m_clear_values.size())
+            .setPClearValues(m_clear_values.data());
+    }
+
+    void RenderPass::set_render_area(const vk::Rect2D& render_area)
+    {
+        m_render_area = render_area;
+        m_begin_info.setRenderArea(m_render_area);
+    }
+
+    void RenderPass::begin(const vk::CommandBuffer& command_buffer, const vk::Framebuffer& framebuffer)
+    {
+        m_begin_info.setFramebuffer(framebuffer);
+        command_buffer.beginRenderPass(&m_begin_info, vk::SubpassContents::eInline);
+    }
+
+    void RenderPass::end(const vk::CommandBuffer& command_buffer) const
+    {
+        command_buffer.endRenderPass();
+    }
+
+    void RenderPass::execute(const vk::CommandBuffer& command_buffer, const vk::Framebuffer& framebuffer,
+                             const std::function<void(const vk::CommandBuffer&)>& lambda)
+    {
+        begin(command_buffer, framebuffer);
+        lambda(command_buffer);
+        end(command_buffer);
+    }
+
+
 }
