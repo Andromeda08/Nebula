d6c4344fdc10b02f4c6f61ebe9dde6376a09ec8d
Andromeda08 <vkovacsbalazs26@gmail.com>
2024-04-16 13:21:37 +0200

    OptimizedCompiler: first 3 steps

--- Nebula/nebula/nrg/compiler/optimized/OptimizedCompiler.cpp
+++ Nebula/nebula/nrg/compiler/optimized/OptimizedCompiler.cpp
@@ -1,6 +1,96 @@
 #include "OptimizedCompiler.hpp"
 
+#include <format>
+#include <sstream>
+#include "ResourceOptimizer.hpp"
+
 namespace Nebula::nrg
 {
+    OptimizedCompiler::OptimizedCompiler(const std::shared_ptr<Context>& context)
+    : CompilerStrategy(context)
+    {
+    }
 
+    CompilerResult OptimizedCompiler::compile(const Graph& graph)
+    {
+        auto  nodes = graph.get_nodes_vector();
+        auto& edges = graph.edges;
+
+        CompilerResult result = {};
+        std::vector<std::string>& logs = result.internal_logs;
+
+        result.start_timestamp = std::chrono::utc_clock::now();
+        logs.push_back(std::format("Compiling started at {:%Y-%m-%d %H:%M}", result.start_timestamp));
+        logs.push_back(fmt_nodes_str(std::format("Input Nodes ({}):", nodes.size()), nodes));
+
+        // 1. Cull unreachable nodes
+        std::vector<node_ptr> connected_nodes;
+        try {
+            connected_nodes = filter_unreachable_nodes(nodes);
+        }
+        catch (const std::runtime_error& ex) {
+            make_failed_result(result, ex.what());
+            return result;
+        }
+
+        std::size_t culled_node_cnt = nodes.size() - connected_nodes.size();
+        if (culled_node_cnt != 0)
+        {
+            logs.push_back(std::format("Found and culled {} unreachable node(s).", culled_node_cnt));
+        }
+        else
+        {
+            logs.emplace_back("No unreachable nodes were found.");
+        }
+
+        logs.push_back(fmt_nodes_str(std::format("Remaining Nodes ({}):", connected_nodes.size()), connected_nodes));
+
+        // 2. Execution order
+        std::vector<node_ptr> execution_order;
+        try {
+            execution_order = get_execution_order(connected_nodes);
+        }
+        catch (const std::runtime_error& ex) {
+            make_failed_result(result, ex.what());
+            return result;
+        }
+
+        logs.push_back(fmt_nodes_str(std::format("Execution order: ({}):", execution_order.size()), execution_order));
+
+        // 3. Resource optimization
+        ResourceOptimizerOptions optimizer_options {
+            .export_result = true,
+        };
+        ResourceOptimizerResult optimizer_result;
+        auto resource_optimizer = std::make_shared<ResourceOptimizer>(execution_order, edges, optimizer_options);\
+
+        try {
+            optimizer_result = resource_optimizer->run();
+        }
+        catch (const std::runtime_error& ex) {
+            make_failed_result(result, ex.what());
+            return result;
+        }
+
+        // 4. Create Resources
+
+        return result;
+    }
+
+    std::string OptimizedCompiler::fmt_nodes_str(const std::string& prefix, const std::vector<node_ptr>& nodes)
+    {
+        std::stringstream input_nodes_str;
+        input_nodes_str << prefix;
+        for (const auto& node : nodes)
+        {
+            input_nodes_str << std::format(" [{}]", node->name());
+        }
+        return input_nodes_str.str();
+    }
+
+    void OptimizedCompiler::make_failed_result(CompilerResult& result, const std::string& error_message)
+    {
+        result.success = false;
+        result.failure_message = error_message;
+    }
 }
--- Nebula/nebula/nrg/compiler/optimized/OptimizedCompiler.hpp
+++ Nebula/nebula/nrg/compiler/optimized/OptimizedCompiler.hpp
@@ -10,11 +10,18 @@ namespace Nebula::nrg
 {
     class OptimizedCompiler : public CompilerStrategy
     {
+        using node_ptr = std::shared_ptr<EditorNode>;
+
     public:
         explicit OptimizedCompiler(const std::shared_ptr<Context>& context);
 
         CompilerResult compile(const Graph& graph) override;
 
         ~OptimizedCompiler() override = default;
+
+    private:
+        static void make_failed_result(CompilerResult& result, const std::string& error_message);
+
+        static std::string fmt_nodes_str(const std::string& prefix, const std::vector<node_ptr>& nodes);
     };
 }
